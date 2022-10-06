28585309a3805b9f17241e58466206a5e666f71f
unknown <11819900>
2022-10-06 17:38:44 -0300

    Implemented models. Tested basic CRUD functionality.

--- Caqui/flight_management/models.py
+++ Caqui/flight_management/models.py
@@ -1,3 +1,45 @@
+from asyncio.windows_events import NULL
+from secrets import choice
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from flight_management.enums import AirportCodes, Status, Role  
 
-# Create your models here.
+# tx = text
+# dt = datetime
+# nm = enums 
+# fk = foreign key
+
+
+# TODO: verificar passagem de timezone
+
+class FlightStatus(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    dt_departure = models.DateTimeField(blank=True, null=True)
+    dt_arrival = models.DateTimeField(blank=True, null=True)
+    nm_status = models.CharField(max_length=10, choices=Status.choices, default=0)
+    dt_creation = models.DateTimeField(auto_now=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    dt_done = models.DateTimeField(blank=True, null=True)
+    class Meta:
+        db_table = 'flight_status'
+
+class Flight(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    fk_flightstatus = models.ForeignKey(FlightStatus, on_delete=models.SET_NULL, blank=True, null=True)
+    tx_code = models.CharField(max_length=8)
+    dt_est_departure = models.DateTimeField()
+    dt_est_arrival = models.DateTimeField()
+    nm_origin = models.CharField(max_length=3, choices=AirportCodes.choices)
+    nm_destination = models.CharField(max_length=3, choices=AirportCodes.choices)
+    class Meta:
+        db_table = 'flight'
+
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tx_name = models.CharField(max_length=42)
+    nm_role = models.CharField(max_length=24, choices=Role.choices)
+    tx_username = models.CharField(max_length=42)
+    tx_hash_key = models.CharField(max_length=64)
+    class Meta:
+        db_table = 'user'
--- Caqui/flight_management/tests.py
+++ Caqui/flight_management/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from datetime import datetime
+from flight_management.models import Flight, FlightStatus, User
+from flight_management.enums import AirportCodes, Status, Role  
 
-# Create your tests here.
+class FlightCRUDTest(TestCase):
+    def setUp(self):
+        Flight.objects.create(tx_code="GOL0000", dt_est_departure=datetime.now(), dt_est_arrival=datetime.now(), nm_origin="BSB", nm_destination="CGH", fk_flightstatus=FlightStatus.objects.create())
+
+    def testCreated(self):
+        voo = Flight.objects.get(tx_code="GOL0000")
+        self.assertEqual(voo.tx_code, "GOL0000")
+
+    def testUpdated(self):
+        Flight.objects.filter(id=1).update(nm_destination=AirportCodes.GRU)
+        voo = Flight.objects.get(tx_code="GOL0000")
+        self.assertEqual(voo.nm_destination, str(AirportCodes.GRU.value))
+
+    def testDeleted(self):
+        sizeOrig = len(Flight.objects.all())
+        Flight.objects.filter(id=1).delete()        
+        sizeEnd = len(Flight.objects.all())
+        self.assertEqual(sizeEnd, sizeOrig-1)
+
+class FlightStatusCRUDTest(TestCase):
+    def setUp(self):
+        FlightStatus.objects.create(nm_status=Status.PREVISTO)
+
+    def testCreated(self):
+        status = FlightStatus.objects.get(nm_status=Status.PREVISTO)
+        self.assertEqual(status.nm_status, str(Status.PREVISTO.value))
+
+    def testUpdated(self):
+        FlightStatus.objects.filter(id=1).update(nm_status=Status.EMBARCANDO)
+        status = FlightStatus.objects.get(id=1)
+        self.assertEqual(status.nm_status, str(Status.EMBARCANDO.value))
+
+    def testDeleted(self):
+        sizeOrig = len(FlightStatus.objects.all())
+        FlightStatus.objects.filter(id=1).delete()        
+        sizeEnd = len(FlightStatus.objects.all())
+        self.assertEqual(sizeEnd, sizeOrig-1)
+
+class UserCRUDTest(TestCase):
+    def setUp(self):
+        User.objects.create(tx_name="Izabela", nm_role=Role.GERENTE_DE_OPERACOES, tx_username="iza", tx_hash_key="905492")
+
+    def testCreated(self):
+        user = User.objects.get(tx_username="iza")
+        self.assertEqual(user.tx_username, "iza")
+
+    def testUpdated(self):
+        User.objects.filter(id=1).update(tx_username="isa")
+        user = User.objects.get(tx_name="Izabela")
+        self.assertEqual(user.tx_username, "isa")
+
+    def testDeleted(self):
+        sizeOrig = len(User.objects.all())
+        User.objects.filter(id=1).delete()        
+        sizeEnd = len(User.objects.all())
+        self.assertEqual(sizeEnd, sizeOrig-1)
