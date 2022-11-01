b48e368382701b41e23e2da71f76702bc47cd49e
Lucas Ramos <lucasret22@gmail.com>
2022-10-31 21:01:06 -0300

    improvement update status

--- Caqui/flight_management/forms.py
+++ Caqui/flight_management/forms.py
@@ -5,6 +5,7 @@ class DateInput(forms.DateTimeInput):
     input_type = 'datetime-local'
 
 class FormUpdateStatus(forms.Form):
+
     code_flight = forms.CharField(label='Código de Voo', max_length=8, required=False)
     date_departure = forms.DateTimeField(label='Data de Partida', widget=DateInput, required=False)
     date_arrival = forms.DateTimeField(label='Data de Chegada', widget=DateInput, required=False)
