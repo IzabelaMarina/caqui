624d669482c508c9168c51cffe7ac681649cdbba
Lucas Ramos <lucasret22@gmail.com>
2022-11-07 12:07:31 -0300

    bugfix screens for different roles

--- Caqui/flight_monitoring/views.py
+++ Caqui/flight_monitoring/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render
 from datetime import datetime
 from flight_management.enums import Role
 from flight_management.models import FlightStatus, Flight, User
+from login import views
 #from django.core import serializers
 
 # Create your views here.
-def flightmonitoringview(request,selectedRole):
+def flightmonitoringview(request):
     all_flights = Flight.objects.all()
     context = {
         'all_flights' : all_flights,
-    }
-    return render(request, "home.html", context)
-
-def flightmonitoringview_temporary(request):
-    all_flights = Flight.objects.all()
-    context = {
-        'all_flights' : all_flights,
+        'selectedRole' : str(views.role)
     }
     return render(request, "home.html", context)
