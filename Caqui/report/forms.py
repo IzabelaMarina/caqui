5a13034d719790b1fd5e26a5408ca2f7b9725472
IzabelaMarina <83678692+IzabelaMarina@users.noreply.github.com>
2022-12-01 16:33:32 -0300

    Add date validation on reports forms

--- Caqui/report/forms.py
+++ Caqui/report/forms.py
@@ -1,13 +1,46 @@
 from django import forms
-from django import forms
 from django.forms import DateInput
+import datetime
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import pytz
+utc=pytz.UTC
 
 class DateInput(forms.DateTimeInput):
     input_type = 'datetime-local'
 
 class FormDates(forms.Form):
-    min_date = forms.DateTimeField(label='Data de início', widget=forms.DateInput(attrs={'type': "date", 'class': "form-control", 'placeholder':"dd/mm/yyyy"}), required=False)
-    max_date = forms.DateTimeField(label='Data de fim', widget=forms.DateInput(attrs={'type': "date", 'class': "form-control", 'placeholder':"dd/mm/yyyy"}), required=False)
+    min_date = forms.DateTimeField(label='Data de início', widget=forms.DateInput(attrs={'type': "date", 'class': "form-control", 'placeholder':"dd/mm/yyyy"}), required=True)
+    max_date = forms.DateTimeField(label='Data de fim', widget=forms.DateInput(attrs={'type': "date", 'class': "form-control", 'placeholder':"dd/mm/yyyy"}), required=True)
 
-class FormCode(forms.Form):
-    flight_code = forms.CharField(label='Código de Voo', max_length=8, widget = forms.TextInput(attrs={'type': "text", 'class': "form-control", 'placeholder':"Insira um código de voo"}), required=False)
+    def clean_min_date(self):
+        data = self.cleaned_data['min_date']
+        data = data.replace(tzinfo=utc)
+
+        # Check date is not in future.
+        if data > utc.localize(datetime.datetime.now()):
+            raise ValidationError(_('Data inválida. Relatórios não podem ser gerados para datas futuras.'))
+
+        # Remember to always return the cleaned data.
+        return data
+
+    def clean_max_date(self):
+        data = self.cleaned_data['max_date']
+        data = data.replace(tzinfo=utc)
+
+        # Check date is not in future.
+        if data > utc.localize(datetime.datetime.now()):
+            raise ValidationError(_('Data inválida. Relatórios não podem ser gerados para datas futuras.'))
+        
+        # Remember to always return the cleaned data.
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        min_date = cleaned_data.get("min_date")
+        max_date = cleaned_data.get("max_date")
+
+        if min_date and max_date:
+            if max_date < min_date:
+                raise ValidationError(_('Data final não pode ser menor do que a data inicial!'))            
+        return cleaned_data
--- Caqui/report/urls.py
+++ Caqui/report/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from report import views
 
 urlpatterns = [
-    path('', views.ReportView.as_view()),
+    path('', views.ReportView.reportview),
     path('flightreport', views.reportflightview),
     path('export-pdf', views.export_pdf),
     path('export-pdf-cmpny', views.export_pdf_cmpny),
